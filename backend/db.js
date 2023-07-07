06cbf34fc34daf79f2b5318519a6225246454de3
shubham kumar <clboy768@gmail.com>
2023-07-07 19:18:45 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false)
-const mongoUri ="mongodb://0.0.0.0:27017/Notebook"
-const connectToMongo= ()=>{
-    mongoose.connect(mongoUri,()=>{
-        console.log("connected to mongoose successfully")
-    })
-}
-module.exports= connectToMongo;
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+// const mongoUri ="mongodb://0.0.0.0:27017/Notebook"
+const mongoUri =
+  "mongodb+srv://shubhamfarainzi:ExOuXCt5tWw8oHB@cluster0.dxkisay.mongodb.net/Notebook?retryWrites=true&w=majority";
+const connectToMongo = () => {
+  mongoose.connect(mongoUri, () => {
+    console.log("connected to mongoose successfully");
+  });
+};
+module.exports = connectToMongo;
