2f52903544ab34de3b99a718f80177fe8852c7df
shubham kumar <clboy768@gmail.com>
2023-07-07 20:21:03 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,9 @@ const port = process.env.PORT ||3001;
 app.use(express.json())
 
 // available routes;
-app.use("/",(req,res)=>{
-    res.json({message:"Hello Home Page"})
-});
+// app.use("/",(req,res)=>{
+//     res.json({message:"Hello Home Page"})
+// });
 app.use('/api/auth',require('./routes/auth.js'))
 app.use('/api/notes',require('./routes/notes.js'))
 app.listen(port,()=>{
