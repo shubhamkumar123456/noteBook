b8138138190ac25844eb2cb094b404c1ea006599
shubham kumar <clboy768@gmail.com>
2023-07-07 20:43:09 +0530

    update env

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 // const mongoUri ="mongodb://0.0.0.0:27017/Notebook"
-const mongoUri =
-  "mongodb+srv://shubhamfarainzi:ExOuXCt5tWw8oHB@cluster0.dxkisay.mongodb.net/Notebook?retryWrites=true&w=majority";
+const mongoUri =process.env.MONGODB_URL
 const connectToMongo = () => {
   mongoose.connect(mongoUri, () => {
     console.log("connected to mongoose successfully");
